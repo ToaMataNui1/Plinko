c599347ceacbcd9fa082d2202d93bbc655685afb
Srinidhaya Vempati <103005437+ToaMataNui1@users.noreply.github.com>
2023-06-04 15:58:23 -0400

    Add files via upload

---
+++ Lab_Plinko.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Lab_Plinko {
+    public static void dropMarble(int[] slots) {
+        int pegs = slots.length - 1;
+        for (int i = 0; i < pegs; i++) {
+            int rand = (int) (Math.random() * 2) + 0;
+    
+            if (rand == 0) {
+                System.out.print('L');
+            } else if (rand == 1) {
+                System.out.print('R');
+            }
+    
+        }
+
+        int randomIndex = (int)(Math.random() * slots.length);
+        slots[randomIndex]++;
+        
+        System.out.println();
+    }
+    
+
+    public static void printBoard(int[] slots){
+        System.out.print("|");
+        for (int i = 0; i < slots.length; i++) {
+            System.out.print(slots[i] + "|");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // most of the program, handles user input
+        // make appropriate calls to dropMarble and printBoard 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to Plinko!");
+        System.out.println("How many slots in the board?");
+        
+        int numSlots = input.nextInt();
+        int[] slots = new int[numSlots];
+
+        System.out.println("How many marbles to drop?");
+        int numBallDrop = input.nextInt();
+
+        while (numBallDrop != 0) { 
+            do {
+                dropMarble(slots);
+                numBallDrop--;
+            } while (numBallDrop > 0);
+            printBoard(slots);
+
+            System.out.println("How many balls to drop?");
+            numBallDrop = input.nextInt();
+
+            if (numBallDrop == 0) {
+                System.out.println("That's the end!");
+                input.close();
+            }
+        }
+    }
+}
